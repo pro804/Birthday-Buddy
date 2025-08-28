fe104e8a0a2357c6eb7d0102a08fd680e4404712
pro804 <petsoulis.appdev@gmail.com>
2025-08-28 15:19:42 +0300

    feat: implement data import, useState hook, and render list with Person and List components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import data from "./data";
+import List from "./List";
+
 function App() {
-  return <h2>Birthday Reminder</h2>;
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/Person.jsx
@@ -0,0 +1,13 @@
+const Person = ({ image, name, age }) => {
+  return (
+    <article>
+      <img src={image} alt={name} style={{ width: "10rem" }} />
+      <div>
+        <h4>{name}</h4>
+        <p>{age} years</p>
+      </div>
+    </article>
+  );
+};
+
+export default Person;
